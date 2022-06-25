29fb517e367ed475eb5895d2cdc9c2b35f9a7639
Maxigong <maxi@nolgong.com>
2022-06-25 14:50:49 +0900

    fix useRefs

--- src/useRefs.ts
+++ src/useRefs.ts
@@ -1,62 +1,67 @@
 class Ref {
-  ref: HTMLFormElement | HTMLElement | HTMLInputElement | HTMLDivElement;
-  constructor(ref: any) {
-    this.ref = ref;
-  }
+    ref: HTMLFormElement | HTMLElement | HTMLInputElement | HTMLDivElement;
+    constructor(ref: any) {
+        this.ref = ref;
+    }
 
-  onClick(callback: () => void) {
-    if (this.ref) {
-      this.ref.addEventListener('click', callback);
+    onClick(callback: () => void) {
+        if (this.ref) {
+            this.ref.addEventListener("click", callback);
+        }
     }
-  }
-  onChange(callback: () => void) {
-    if (this.ref) {
-      this.ref.addEventListener('keyup', callback);
+    onChange(callback: () => void) {
+        if (
+            (this.ref && (this.ref as HTMLInputElement).type === "text") ||
+            this.ref.nodeName === "FORM"
+        ) {
+            this.ref.addEventListener("keyup", callback);
+        } else {
+            this.ref.addEventListener("change", callback);
+        }
     }
-  }
-  onSubmit(callback: () => void) {
-    if (this.ref) {
-      this.ref.addEventListener('submit', callback);
+    onSubmit(callback: () => void) {
+        if (this.ref) {
+            this.ref.addEventListener("submit", callback);
+        }
     }
-  }
-  onSubmitPrevent(callback: () => void) {
-    if (this.ref) {
-      this.ref.addEventListener('submit', (e) => {
-        e.preventDefault();
-        callback();
-      });
+    onSubmitPrevent(callback: () => void) {
+        if (this.ref) {
+            this.ref.addEventListener("submit", (e) => {
+                e.preventDefault();
+                callback();
+            });
+        }
     }
-  }
-  toogle(className: string) {
-    if (this.ref) {
-      this.ref.classList.toggle(className);
+    toogle(className: string) {
+        if (this.ref) {
+            this.ref.classList.toggle(className);
+        }
     }
-  }
 }
 
 export const refs = (id: string): any => {
-  const refs = document.querySelectorAll(`#${id}`);
-  let element;
-  let list = [] as any[];
-  if (!refs) {
-    alert(`Element with id ${id} not found`);
-    throw new Error(`Element with id ${id} not found`);
-  } else {
-    refs.forEach((refel) => {
-      element = new Ref(refel);
-      list.push(element);
-    });
-    return list;
-  }
+    const refs = document.querySelectorAll(`#${id}`);
+    let element;
+    let list = [] as any[];
+    if (!refs) {
+        alert(`Element with id ${id} not found`);
+        throw new Error(`Element with id ${id} not found`);
+    } else {
+        refs.forEach((refel) => {
+            element = new Ref(refel);
+            list.push(element);
+        });
+        return list;
+    }
 };
 
 export const ref = (id: string): any => {
-  const ref = document.getElementById(id);
+    const ref = document.getElementById(id);
 
-  if (!ref) {
-    alert(`Element with id ${id} not found`);
-    throw new Error(`Element with id ${id} not found`);
-  } else {
-    return new Ref(ref);
-  }
+    if (!ref) {
+        alert(`Element with id ${id} not found`);
+        throw new Error(`Element with id ${id} not found`);
+    } else {
+        return new Ref(ref);
+    }
 };
